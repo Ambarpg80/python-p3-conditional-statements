81fcd79866bd3d2ecd80244fa50b472438d4b688
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-08-05 12:46:53 -0400

    add solution

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python3
+import ipdb
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username == "admin" and password == "12345":
+        return "Access granted"
+    elif username == "ADMIN" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    msg = "It's perfect out there!"
+    if temperature < 40:
+        msg = "It's brisk out there!"
+    elif temperature > 40 and temperature < 65:
+        msg = "It's a little chilly out there!"
+    elif temperature > 85:
+        msg = "It's too dang hot out there!" 
+    return msg
 
 def fizzbuzz(num):
-    # your code here
-    pass
-
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 5 == 0:
+        return "Buzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    else:
+        return num
+    
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return  num1 + num2
+    elif operation == "-":
+        return  num1 - num2
+    elif operation ==  "*":
+        return  num1 * num2
+    elif operation == "/":
+        return  num1 / num2
+    else:
+        print("Invalid operation!") 
+        return None
